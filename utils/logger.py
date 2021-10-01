179759b2dc944b6610062d353cc468a38d1b9545
Joshith Rayaroth Koderi (jork) <jork@cisco.com>
2021-09-30 20:16:30 -0400

    move utils

---
+++ utils/logger.py
@@ -0,0 +1,45 @@
+'''
+ Wolfinch Auto trading Bot
+ Desc: Generic Logging routines
+#  Copyright: (c) 2017-2020 Joshith Rayaroth Koderi
+#  This file is part of Wolfinch.
+# 
+#  Wolfinch is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+# 
+#  Wolfinch is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with Wolfinch.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+import logging
+
+# 
+# class WolfinchLogger (logging):
+#     
+# class Logger():
+#     def __init__(self, name=None):
+#         self.getLogger (name)
+#             
+def getLogger (name):
+#     FORMAT = "[%(levelname)s:%(name)s:%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
+    FORMAT = "[%(asctime)s %(levelname)s:%(name)s - %(funcName)20s(%(lineno)d) ] %(message)s"
+
+#     logging.basicConfig(filename='wolfinch.log', filemode='a', level=logging.DEBUG, format=FORMAT, datefmt='%Y-%m-%d %H:%M:%S')   
+    logging.basicConfig(level=logging.DEBUG, format=FORMAT, datefmt='%Y-%m-%d %H:%M:%S')     
+    log = logging.getLogger(name)
+    log.CRITICAL =  logging.CRITICAL
+    log.ERROR    =  logging.ERROR
+    log.WARNING  =  logging.WARNING
+    log.INFO     =  logging.INFO
+    log.DEBUG    =  logging.DEBUG
+    log.NOTSET   =  logging.NOTSET
+    return log
+
+#EOF
---
+++ utils/utils.py
@@ -0,0 +1,58 @@
+#  Copyright: (c) 2017-2020 Joshith Rayaroth Koderi
+#  This file is part of Wolfinch.
+# 
+#  Wolfinch is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+# 
+#  Wolfinch is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with Wolfinch.  If not, see <https://www.gnu.org/licenses/>.
+
+import networkx as networkx
+import numpy
+from deap.tools import Statistics
+# from matplotlib import pyplot as plt
+from termcolor import colored
+
+from conf import runid
+from objective_function import soft_maximum_worst_case
+
+
+def draw(history, toolbox):
+    ax = plt.figure()
+    ax.set_figheight(30)
+    ax.set_figwidth(30)
+    graph = networkx.DiGraph(history.genealogy_tree)
+    graph = graph.reverse()  # Make the grah top-down
+    colors_inds = (history.genealogy_history[i] for i in graph)
+    colors = [soft_maximum_worst_case(ind) if ind.fitness.valid else -10 for ind in colors_inds]
+
+    positions = networkx.drawing.nx_agraph.graphviz_layout(graph, prog="dot")
+
+    networkx.draw(graph, positions, node_color=colors, ax=ax.add_subplot(111), figsize=(30, 30), node_size=150)
+    ax.savefig('logs/history/{runid}.png'.format(runid=runid))
+
+
+def log_stuff(g, history, hof, population, stats):
+    # draw(history, toolbox)
+    record = stats.compile(population)
+    hof.update(population)
+    hof.persist()
+    print(colored('\nGeneration {g} {record}', 'green') )
+    # print(hof)
+
+
+def statsa():
+    stats = Statistics(key=lambda ind: soft_maximum_worst_case(ind))
+    stats.register("avg", numpy.mean)
+    stats.register("std", numpy.std)
+    stats.register("min", numpy.min)
+    stats.register("max", numpy.max)
+    stats.register("len", len)
+    return stats
