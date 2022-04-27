83b62071fe6f4d007b8215011269c9829975fb4b
ldmonko <joe.cet@gmail.com>
2022-04-27 13:31:15 -0700

    opt_oi_spike strategy imp

--- strategies/opt_oi_spike.py
+++ strategies/opt_oi_spike.py
@@ -206,7 +206,6 @@ class OPT_OI_SPIKE(Screener):
                     cp_pct = 0 if (oi_c_d - oi_p_d) == 0 else round(oi_c_d/(oi_c_d - oi_p_d), 2)
                 else:
                     cp_pct = 0 if oi_p_d == 0 else round(oi_c_d/(oi_p_d), 2)
-                cp_pct = 0 if (oi_c_d + oi_p_d) == 0 else 
                 calls_puts_d = "%d/%d(%s)"%(oi_c_d, oi_p_d, cp_pct)
                 high_calls_oi = str(os_c["high_calls_oi"])+"@"+str(os_c["high_calls_strike"])+"/"+str(os_c["high_calls_exp"])
                 high_puts_oi = str(os_c["high_puts_oi"])+"@"+str(os_c["high_puts_strike"])+"/"+str(os_c["high_puts_exp"])
